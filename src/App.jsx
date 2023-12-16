730a5099fb63e69ac83df1fcdf2ad87560382f19
Olivia <ikajiubachidiogo@gmail.com>
2023-12-16 23:40:35 +0100

    Login page completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import {UserSignUpForm} from "./components/auth/UserSignUpForm.jsx";
+import {Login} from "./components/auth/Login.jsx";
 
 function App() {
 
   return (
     <>
         <UserSignUpForm />
+        <Login/>
     </>
   )
 }
