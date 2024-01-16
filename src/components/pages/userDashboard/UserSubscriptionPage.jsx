0ea801afdcd7b8587adc161ce18f47af09d40d81
Praise Udoh <praiseubong28@gmail.com>
2024-01-17 00:11:01 +0100

    subscription page

--- src/components/pages/userDashboard/UserSubscriptionPage.jsx
+++ src/components/pages/userDashboard/UserSubscriptionPage.jsx
@@ -3,7 +3,20 @@ import { PricingCard } from "./PricingCard";
 
 export const UserSubscriptionPage = () => {
   return (
-  
+  <div>
+      <span className="items-center flex w-[59px] max-w-full gap-1">
+          <img
+            loading="lazy"
+            src="src/assets/images/userCatImages/arrow_back.png"
+            className="aspect-square object-contain object-center w-full overflow-hidden shrink-0 flex-1 my-auto"
+          />
+          <div className="text-black text-center text-sm leading-7 self-stretch grow whitespace-nowrap">
+            Back
+          </div>
+      </span>
+      <div className="text-black text-4xl font-bold leading-[56px] tracking-wide self-stretch mt-7 max-md:max-w-full">
+          My subscriptions
+       </div>
     <div className="flex justify-center gap-9">
         {/* First PricingCard */}
         <PricingCard
@@ -39,5 +52,6 @@ export const UserSubscriptionPage = () => {
         />
 
     </div>
+  </div>
   );
 };
