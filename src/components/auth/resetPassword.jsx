155dd255f5e0a27e86870c5b8cade33acf2f20d0
EngrCharles042 <obicharles94@gmail.com>
2024-02-21 17:42:49 +0100

    update for page response

--- src/components/auth/resetPassword.jsx
+++ src/components/auth/resetPassword.jsx
@@ -88,44 +88,44 @@ export const ResetPassword = ({
   };
 
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className="flex">
       <div>
         <img
           loading="lazy"
           srcSet={authImage}
-          className="flex-shrink-0 object-cover md:object-fill flex-1"
+          className="flex-shrink-0 object-cover flex-1"
           alt=""
         />
       </div>
-      <div className="flex-1 justify-center items-center bg-emerald-200 flex flex-col px-16 py-12 md:px-5">
+      <div className="justify-center items-center bg-emerald-200 flex flex-col px-16 py-12 max-md:px-5">
         {successCard && <PasswordResetSuccessCard email={email} />}
         {successCard && (
-          <div className="absolute bg-black inset-0 opacity-80"></div>
+          <div className="absolute bg-black inset-[0] opacity-[0.8]"></div>
         )}
 
-        <div className="w-full md:max-w-md lg:max-w-lg xl:max-w-xl">
+        <div>
           <form
             onSubmit={handleSubmit}
-            className="shadow-lg bg-white flex flex-col mt-16 mb-11 px-11 pb-24 pt-16 rounded-xl max-md:mt-10 max-md:px-5"
+            className="shadow-lg bg-white flex w-[35.25rem] max-w-full flex-col mt-[15vh] mb-11 px-11 pb-[6rem] pt-[4rem] rounded-xl max-md:my-10 max-md:px-5"
           >
-            <div className="flex items-center gap-1.5">
+            <div className="items-stretch self-center flex gap-1.5">
               <img
                 loading="lazy"
                 srcSet={logo}
-                className="aspect-square object-contain object-center w-10 md:w-12 lg:w-14 xl:w-16 overflow-hidden"
+                className="aspect-square object-contain object-center w-[41px] overflow-hidden shrink-0 max-w-full"
                 alt=""
               />
-              <div className="text-green-500 text-xl font-semibold leading-7 self-center">
+              <div className="text-green-500 text-xl font-semibold leading-7 self-center grow whitespace-nowrap my-auto">
                 <span className="text-black">Books</span>
                 <span className="text-green-500">Ville</span>
               </div>
             </div>
 
-            <div className="text-gray-900 text-2xl font-bold leading-8 self-center mt-4">
+            <div className="text-gray-900 text-2xl font-bold leading-8 self-center whitespace-nowrap mt-2.5">
               Reset your password
             </div>
 
-            <div className="text-neutral-800 text-sm flex flex-col font-medium leading-5 self-stretch mt-7">
+            <div className="text-neutral-800 text-sm flex flex-col font-medium leading-5 self-stretch mt-7 max-md:max-w-full">
               <label htmlFor="password">Password</label>
               <input
                 type={showPassword ? "text" : "password"}
@@ -135,17 +135,17 @@ export const ResetPassword = ({
                 onChange={handleChange}
                 required
                 autoComplete="password"
-                className="text-neutral-500 text-base leading-6 self-stretch rounded border border-[color:var(--Gray-3,#828282)] bg-white mt-1 pl-3 pr-12 py-3"
+                className="text-neutral-500 text-base leading-6 whitespace-nowrap self-stretch rounded border border-[color:var(--Gray-3,#828282)] bg-white justify-center mt-1 pl-3 pr-16 py-3 border-solid items-start max-md:max-w-full max-md:pr-5"
               />
               <span
-                className="password-toggle relative bottom-[2rem] left-[20rem] text-green-700"
+                className="password-toggle relative bottom-[2rem] left-[28rem] text-green-700"
                 onClick={handleTogglePassword}
               >
                 {showPassword ? <FaEyeSlash /> : <FaEye />}
               </span>
             </div>
 
-            <div className="text-neutral-800 text-sm flex flex-col font-medium leading-5 self-stretch">
+            <div className="text-neutral-800 text-sm flex flex-col font-medium leading-5 self-stretch max-md:max-w-full">
               <label htmlFor="confirmNewPassword">Confirm Password</label>
               <input
                 type={showConfirmPassword ? "text" : "password"}
@@ -155,10 +155,10 @@ export const ResetPassword = ({
                 onChange={handleChange}
                 required
                 autoComplete="confirm-password"
-                className="text-neutral-500 text-base leading-6 self-stretch rounded border border-[color:var(--Gray-3,#828282)] bg-white mt-1 pl-3 pr-12 py-3"
+                className="text-neutral-500 text-base leading-6 whitespace-nowrap self-stretch rounded border border-[color:var(--Gray-3,#828282)] bg-white justify-center mt-1 pl-3 pr-16 py-3 border-solid items-start max-md:max-w-full max-md:pr-5"
               />
               <span
-                className="confirmPassword-toggle relative bottom-[2rem] left-[20rem] text-green-700"
+                className="confirmPassword-toggle relative bottom-[2rem] left-[28rem] text-green-700"
                 onClick={handleToggleConfirmPassword}
               >
                 {showConfirmPassword ? <FaEyeSlash /> : <FaEye />}
@@ -167,7 +167,7 @@ export const ResetPassword = ({
 
             <button
               type="submit"
-              className="cursor-pointer h-10 hover:bg-black text-gray-50 text-base font-semibold leading-4 bg-green-500 self-stretch mt-2 px-16 py-3 rounded-xl"
+              className="cursor-pointer h-[2.5rem] hover:bg-black text-gray-50 text-base font-semibold leading-4 whitespace-nowrap flex justify-center items-center bg-green-500 self-stretch mt-1.5 px-16 py-3 rounded-xl max-md:max-w-full max-md:px-5"
             >
               {!clip ? (
                 "Reset my Password"
@@ -176,7 +176,7 @@ export const ResetPassword = ({
               )}
             </button>
 
-            <div className="text-green-500 text-sm leading-5 self-center mt-2">
+            <div className="text-green-500 text-sm leading-5 self-center whitespace-nowrap mt-2">
               <span className=" text-gray-400">Go back to </span>
               <span className="cursor-pointer font-semibold text-green-500 underline">
                 <Link to={"/login"}>Sign In</Link>
